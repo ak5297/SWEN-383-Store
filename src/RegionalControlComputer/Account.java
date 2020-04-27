7a13310d0d8bcfc71806aab4f137c918089a0498
Alex Ketavongsa <60409096+ak5297@users.noreply.github.com>
2020-04-26 20:01:36 -0400

    Updated the account class

--- src/RegionalControlComputer/Account.java
+++ src/RegionalControlComputer/Account.java
@@ -1,5 +1,15 @@
 package RegionalControlComputer;
 
+//define all variables and methods to be implemented
 public interface Account {
-    public void createAccount();
+
+    final int UID = 49251;
+
+    void createAccount(int UID);
+
+    void login(int ID);
+    
+    void checkCredentials(int ID);
+    
+    void updateAccount();
 }
