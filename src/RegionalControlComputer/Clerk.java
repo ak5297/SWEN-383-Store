eb7fee03094cb2fac03c028cfd6b163f56100446
Thomas Margosian <tmargo101@gmail.com>
2020-04-26 21:00:34 -0400

    Created methods in Clerk.

--- src/RegionalControlComputer/Clerk.java
+++ src/RegionalControlComputer/Clerk.java
@@ -1,7 +1,23 @@
 package RegionalControlComputer;
 
 public class Clerk implements Account {
-    public void createAccount() {
+    public void createAccount(int inNum) {
+
+    }
+
+    public void login(int inId) {
+
+    }
+
+    public void removeAccount() {
+
+    }
+
+    public void checkCredentials(int inId) {
+
+    }
+
+    public void updateAccount() {
 
     }
 }
